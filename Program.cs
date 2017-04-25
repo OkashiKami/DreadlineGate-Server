2c047be356120436a5730a9cb91c4df3a21693df
SinpaiEntertainment <zbigmoney305@gmail.com>
2017-04-25 12:19:37 -0500

    Deposit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dreadline_Gate_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
